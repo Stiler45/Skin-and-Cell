af6644e78cbeb2ca1ccc8bb44b1550ed9ab478c6
Your Name <youremail@example.com>
2025-02-07 19:33:31 +0100

    v9

--- src/app/lander/page.tsx
+++ src/app/lander/page.tsx
@@ -1,13 +1,20 @@
 "use client"
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { First_Section } from "@/app/components/first_seciton"
 
 export default function RedirectPage() {
-  const router = useRouter();
+  /* const router = useRouter();
 
   useEffect(() => {
     router.push('/');
-  }, []);
+  }, []); */
 
-  return null;
+  return( 
+  
+  <main>
+    <First_Section/>
+  </main>
+
+  );
 }
