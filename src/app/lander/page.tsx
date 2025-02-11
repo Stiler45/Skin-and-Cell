39bdc4d9dfe8f1a2371bc961eaf20af0c2855e5d
Your Name <youremail@example.com>
2025-02-11 22:55:01 +0100

    v19

--- src/app/lander/page.tsx
+++ src/app/lander/page.tsx
@@ -50,7 +50,7 @@ export default function RedirectPage() {
 
   return (
 
-    <div className="max-h-[200vh] overflow-y-hidden">
+    <div className="max-h-[200svh] overflow-y-hidden bg-gradient-to-r from-[#719DDF] to-[#7BA1E3]">
       <Image
         src={Bg_Lander}
         alt="Landerbackground"
