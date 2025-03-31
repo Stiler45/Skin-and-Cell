00abf8966c58f82f1ba48a0611139f4870db518d
Stiler45 <gianinsiegrist@gmail.com>
2025-03-31 06:07:48 +0200

    v49

--- src/app/updated/page.tsx
+++ src/app/updated/page.tsx
@@ -281,9 +281,9 @@ const Updated = () => {
                                 <div className="flex justify-center">
                                     <div className="flex flex-col">
                                         <motion.div
-                                            layout layoutId="openSerum" className=" flex justify-center items-center w-[25rem] max-[450px]:w-[20rem] max-[750px]:w-[22rem] relative cursor-pointer p-16 h-[30rem] bg-[#fff] border border-[#000]/10">
+                                            layout layoutId="openSerum-" className=" flex justify-center items-center w-[25rem] max-[450px]:w-[20rem] max-[750px]:w-[22rem] relative cursor-pointer p-16 h-[30rem] bg-[#fff] border border-[#000]/10">
                                             <motion.img
-                                                src="/B3_Serum_Gold.png"
+                                                src="/B3_Serum_gold.png"
                                                 alt="B3 Serum"
                                                 className="h-[320px] max-h-[450px] object-contain"
                                             />
@@ -300,7 +300,7 @@ const Updated = () => {
                                     <div className="flex flex-col">
                                         <motion.div className=" flex justify-center items-center w-[25rem] max-[450px]:w-[20rem] relative cursor-pointer p-16 bg-[#fff] h-[30rem] border border-[#000]/10">
                                             <motion.img
-                                                src="/B3_Eye_Gold.png"
+                                                src="/B3_Eye_gold.png"
                                                 alt="B3 Eye"
                                                 className="h-[320px] max-h-[450px] object-contain"
                                             />
@@ -334,7 +334,7 @@ const Updated = () => {
                                     <div className="flex flex-col">
                                         <motion.div className="flex justify-center items-center w-[25rem] max-[450px]:w-[20rem] relative cursor-pointer p-16 bg-[#fff] h-[30rem] border border-[#000]/10">
                                             <motion.img
-                                                src="/B3_Hand_gold.png"
+                                                src="/B3_Hand_Gold.png"
                                                 alt="B3 Face"
                                                 className="h-[320px] max-h-[450px] object-contain"
                                             />
@@ -351,7 +351,7 @@ const Updated = () => {
                                     <div className="flex flex-col">
                                         <motion.div className="flex justify-center items-center w-[25rem] max-[450px]:w-[20rem] relative cursor-pointer p-16 bg-[#fff] h-[30rem] border border-[#000]/10">
                                             <motion.img
-                                                src="/B3_Hand_gold.png"
+                                            src="/2_B3_Body_slim-200ml.png"
                                                 alt="B3 Body"
                                                 className="h-[320px] max-h-[450px] object-contain"
                                             />
@@ -374,7 +374,7 @@ const Updated = () => {
                                     <motion.div
                                         layout layoutId="openSerum" className=" flex justify-center items-center w-[25rem] max-[750px]:w-[22rem] relative cursor-pointer p-16 h-[30rem] bg-[#fff] border border-[#000]/10">
                                         <motion.img
-                                            src="/B3_Serum_Gold.png"
+                                            src="/B3_Serum_gold.png"
                                             alt="B3 Serum"
                                             className="min-h-[300px] h-[300px] max-h-[320px] object-contain"
                                         />
@@ -389,7 +389,7 @@ const Updated = () => {
                                 <div className="flex flex-col">
                                     <motion.div className=" flex justify-center items-center w-[25rem] relative cursor-pointer p-16 bg-[#fff] h-[30rem] border border-[#000]/10">
                                         <motion.img
-                                            src="/B3_Eye_Gold.png"
+                                            src="/B3_Eye_gold.png"
                                             alt="B3 Eye"
                                             className="min-h-[300px] h-[300px] max-h-[320px] object-contain"
                                         />
@@ -434,7 +434,7 @@ const Updated = () => {
                                 <div className="flex flex-col">
                                     <motion.div className="flex justify-center items-center w-[25rem] relative cursor-pointer p-16 bg-[#fff] h-[30rem] border border-[#000]/10">
                                         <motion.img
-                                            src="/B3_Hand_gold.png"
+                                            src="/B3_Hand_Gold.png"
                                             alt="B3 Hand"
                                             className="min-h-[300px] h-[300px] max-h-[320px] object-contain"
                                         />
